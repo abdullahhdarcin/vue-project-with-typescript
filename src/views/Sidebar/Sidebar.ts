fdc68ed432b2ff53d1e824802ab14458503ae736
abdullahhdarcin <abdullahdarcin01@gmail.com>
2021-10-24 01:49:43 +0300

    edited sidebar folders

---
+++ src/views/Sidebar/Sidebar.ts
@@ -0,0 +1,7 @@
+export default {
+    data(){
+       return {
+          yourLogo : require('@/assets/Logo/SampleLogo.png')
+       }
+    }
+ }
--- src/views/Sidebar/Sidebar.vue
+++ src/views/Sidebar/Sidebar.vue
@@ -1,29 +1,5 @@
-<template>
- <div class="container">
-       <img class="image" v-bind:src="yourLogo" alt="yourLogo">
-        
-         <div class="pages"><span><i class="fas fa-home"></i> Homepage</span></div>
-         <div class="pages"><span><i class="fas fa-users"></i> About</span></div>
-         <div class="pages"><span><i class="fas fa-plus"></i> Add Task</span></div>
+<template src="../Sidebar/Sidebar.html"></template>
 
-         <div class="social">
-            <a href="https://linkedin.com/in/abdullah-dar%C3%A7%C4%B1n/" class="fab fa-linkedin" target="_blank"></a>
-            <a href="https://github.com/abdullahhdarcin" class="fab fa-github" target="_blank"></a>
-         </div>
- </div>
-</template>
+<script lang="ts" src="../Sidebar/Sidebar.ts"></script>
 
-<script>
-export default {
-   data(){
-      return {
-         yourLogo : require('@/assets/Logo/SampleLogo.png')
-      }
-   }
-
-}
-</script>
-
-<style lang="scss" src="../Sidebar/Sidebar.scss" scoped>
-
-</style>
+<style lang="scss" src="../Sidebar/Sidebar.scss" scoped></style>
