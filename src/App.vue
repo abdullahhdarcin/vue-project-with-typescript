89a2ff5b0aeab973f67accb21348e2d5da0e9b90
abdullahhdarcin <abdullahdarcin01@gmail.com>
2021-10-24 01:42:09 +0300

    edited social icons

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
   <router-view/>
 </template>
 
@@ -6,5 +7,7 @@
 #app {
   font-family: 'Sofia Pro', sans-serif;
   @import url('http://fonts.cdnfonts.com/css/sofia-pro');
+  @import url('https://kit.fontawesome.com/6fc105ad23.js');
+  
 }
 </style>
